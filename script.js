7b43bb2ab9a4195e2159af926a6b8c840345d8dd
Júlia Moreira Gregis dos Reis <85226203004@senacrs.edu.br>
2025-04-01 08:17:29 -0300

    oioio

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-btnTrocaCor = document.getElementById("trocaCor");
-let caixaTexto = document.getElementById('caixa');
+const lampada = document.getElementById("lampada")
+const ligado = document.getElementById("ligado")
+const desligado = document.getElementById("desligado")
 
-btnTrocaCor.addEventListener("click", ()=>{
-    let caixaTexto = document.getElementById('caixa');
+ligado.addEventListener("click", function() {
+    lampada.style.backgroundColor = "yellow";
+})
 
-    if(caixaTexto.className === "acende"){
-        caixaTexto.classList.add('apaga');
-        caixaTexto.classList.remove('acende');
-    }else{
-        caixaTexto.classList.add('acende');
-        caixaTexto.classList.remove('apaga');
-    }
-
-    console.log(caixaTexto.className);
-});
-
-const btnTrocaTexto = document.getElementById("trocaTexto");
-btnTrocaTexto.addEventListener("click", () => {
-    if( caixaTexto.innerText === "lampada"){
-        document.getElementById("caixa").innerHTML = "acende";
-        }else{
-            document.getElementById("caixa").innerHTML= "apaga";
-    }
-});
+desligado.addEventListener("click", function() {
+    lampada.style.backgroundColor = "gray";
+})
